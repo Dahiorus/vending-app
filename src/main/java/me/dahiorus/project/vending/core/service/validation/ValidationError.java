2fc3255bf91fe045a02144f1fc0457873c4dfaa6
Dahiorus <brondon.ung@gmail.com>
2022-04-26 13:35:26 +0200

    Separated CommonError from ValidationError

--- src/main/java/me/dahiorus/project/vending/core/service/validation/ValidationError.java
+++ src/main/java/me/dahiorus/project/vending/core/service/validation/ValidationError.java
@@ -26,25 +26,4 @@ public class ValidationError implements Serializable
   {
     return new ValidationError(code, defaultMessage, errorArgs);
   }
-
-  protected enum CommonError
-  {
-    EMPTY_VALUE("validation.constraints.empty_value", "is mandatory"),
-    NOT_UNIQUE("validation.constraints.not_unique", "must be unique");
-
-    final String code;
-
-    final String defaultMessage;
-
-    CommonError(final String code, final String defaultMessage)
-    {
-      this.code = code;
-      this.defaultMessage = defaultMessage;
-    }
-
-    String getDefaultMessageFor(final String field)
-    {
-      return field + " " + defaultMessage;
-    }
-  }
 }
