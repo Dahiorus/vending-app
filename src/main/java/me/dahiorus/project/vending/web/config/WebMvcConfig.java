c21516f0800aed3daad77bf86681a64d6833f5b9
Dahiorus <brondon.ung@gmail.com>
2022-04-28 16:24:19 +0200

    Web config moved in a Configuration class

---
+++ src/main/java/me/dahiorus/project/vending/web/config/WebMvcConfig.java
@@ -0,0 +1,52 @@
+package me.dahiorus.project.vending.web.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import me.dahiorus.project.vending.core.model.AbstractEntity_;
+import me.dahiorus.project.vending.web.api.AppWebService;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer
+{
+  @Bean
+  public OpenAPI openApi()
+  {
+    return new OpenAPI().info(new Info().title("Vending app API")
+      .description("Simple vending application")
+      .version("v1.0"));
+  }
+
+  @Override
+  public void configurePathMatch(final PathMatchConfigurer configurer)
+  {
+    configurer.addPathPrefix("/api", HandlerTypePredicate.forAssignableType(AppWebService.class));
+  }
+
+  @Override
+  public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers)
+  {
+    resolvers.add(pagingHandlerResolver());
+  }
+
+  private static HandlerMethodArgumentResolver pagingHandlerResolver()
+  {
+    PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+    resolver.setOneIndexedParameters(true);
+    resolver.setFallbackPageable(PageRequest.of(0, 20, Sort.by(Direction.DESC, AbstractEntity_.CREATED_AT)));
+
+    return resolver;
+  }
+}
