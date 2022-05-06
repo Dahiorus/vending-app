e06ddb08e0caceabe29cb4f1d621d9350a218892
Dahiorus <brondon.ung@gmail.com>
2022-05-06 22:24:53 +0200

    Transactional set to the DAO interface

--- src/main/java/me/dahiorus/project/vending/core/dao/DAO.java
+++ src/main/java/me/dahiorus/project/vending/core/dao/DAO.java
@@ -10,10 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import me.dahiorus.project.vending.core.exception.EntityNotFound;
 import me.dahiorus.project.vending.core.model.AbstractEntity;
 
+@Transactional(readOnly = true)
 @NoRepositoryBean
 public interface DAO<E extends AbstractEntity> extends JpaRepository<E, UUID>, JpaSpecificationExecutor<E>
 {
-  @Transactional(readOnly = true)
   default E read(final UUID id) throws EntityNotFound
   {
     return findById(id).orElseThrow(() -> new EntityNotFound(getDomainClass(), id));
