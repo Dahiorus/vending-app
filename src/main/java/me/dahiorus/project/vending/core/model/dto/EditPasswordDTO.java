193c3448466b3b21dfab888db24a7a50a476a09c
Dahiorus <brondon.ung@gmail.com>
2022-05-20 00:39:40 +0200

    Add Schema doc

--- src/main/java/me/dahiorus/project/vending/core/model/dto/EditPasswordDTO.java
+++ src/main/java/me/dahiorus/project/vending/core/model/dto/EditPasswordDTO.java
@@ -1,5 +1,9 @@
 package me.dahiorus.project.vending.core.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
+
+@Schema(description = "Request to edit a user's password", accessMode = AccessMode.WRITE_ONLY)
 public record EditPasswordDTO(String oldPassword, String password)
 {
   @Override
