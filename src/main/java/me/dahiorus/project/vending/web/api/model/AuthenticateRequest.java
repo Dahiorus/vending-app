5c5c2db6c6edb7407b67adf1f61f18384a7acc11
Brondon UNG <bung@ilex-international.com>
2022-06-20 16:45:37 +0200

    Corrected request schemas

--- src/main/java/me/dahiorus/project/vending/web/api/model/AuthenticateRequest.java
+++ src/main/java/me/dahiorus/project/vending/web/api/model/AuthenticateRequest.java
@@ -1,8 +1,11 @@
 package me.dahiorus.project.vending.web.api.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 
 @Schema(description = "Request to authenticate a user")
+@SchemaProperty(name = "username", schema = @Schema(example = "username", required = true, description = "The username"))
+@SchemaProperty(name = "password", schema = @Schema(example = "password", required = true, description = "The password"))
 public record AuthenticateRequest(String username, String password)
 {
   @Override
--- src/main/java/me/dahiorus/project/vending/web/api/model/ProvisionRequest.java
+++ src/main/java/me/dahiorus/project/vending/web/api/model/ProvisionRequest.java
@@ -1,5 +1,9 @@
 package me.dahiorus.project.vending.web.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
+
+@SchemaProperty(name = "quantity", schema = @Schema(example = "1", type = "integer", required = true))
 public record ProvisionRequest(int quantity)
 {
   // empty record
