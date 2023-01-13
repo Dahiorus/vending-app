a05555c98ad118d4c56ac7b525ae66cd02cd9e40
Dahiorus <brondon.ung@gmail.com>
2023-01-13 20:39:38 +0100

    Remove useless @Repository annotations

--- src/main/java/me/dahiorus/project/vending/domain/dao/StockDAO.java
+++ src/main/java/me/dahiorus/project/vending/domain/dao/StockDAO.java
@@ -1,10 +1,7 @@
 package me.dahiorus.project.vending.domain.dao;
 
-import org.springframework.stereotype.Repository;
-
 import me.dahiorus.project.vending.domain.model.Stock;
 
-@Repository
 public interface StockDAO extends DAO<Stock>
 {
   // empty interface
--- src/main/java/me/dahiorus/project/vending/domain/dao/VendingMachineDAO.java
+++ src/main/java/me/dahiorus/project/vending/domain/dao/VendingMachineDAO.java
@@ -1,10 +1,7 @@
 package me.dahiorus.project.vending.domain.dao;
 
-import org.springframework.stereotype.Repository;
-
 import me.dahiorus.project.vending.domain.model.VendingMachine;
 
-@Repository
 public interface VendingMachineDAO extends DAO<VendingMachine>
 {
   // empty interface
