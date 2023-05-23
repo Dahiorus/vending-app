aeb267dc5efb8aa51ecbfeed7dd64a44ed029379
Dahiorus <brondon.ung@gmail.com>
2023-05-23 22:55:54 +0200

    Fix deprecated code

--- src/main/java/me/dahiorus/project/vending/web/api/model/AuthenticateRequest.java
+++ src/main/java/me/dahiorus/project/vending/web/api/model/AuthenticateRequest.java
@@ -1,13 +1,14 @@
 package me.dahiorus.project.vending.web.api.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import io.swagger.v3.oas.annotations.media.SchemaProperty;
 
 @Schema(description = "Request to authenticate a user")
 @SchemaProperty(name = "username",
-  schema = @Schema(example = "username", required = true, description = "The username"))
+  schema = @Schema(example = "username", requiredMode = RequiredMode.REQUIRED, description = "The username"))
 @SchemaProperty(name = "password",
-  schema = @Schema(example = "password", required = true, description = "The password"))
+  schema = @Schema(example = "password", requiredMode = RequiredMode.REQUIRED, description = "The password"))
 public record AuthenticateRequest(String username, String password)
 {
   @Override
--- src/main/java/me/dahiorus/project/vending/web/api/model/ProvisionRequest.java
+++ src/main/java/me/dahiorus/project/vending/web/api/model/ProvisionRequest.java
@@ -1,9 +1,11 @@
 package me.dahiorus.project.vending.web.api.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import io.swagger.v3.oas.annotations.media.SchemaProperty;
 
-@SchemaProperty(name = "quantity", schema = @Schema(example = "1", type = "integer", required = true))
+@SchemaProperty(name = "quantity",
+  schema = @Schema(example = "1", type = "integer", requiredMode = RequiredMode.REQUIRED))
 public record ProvisionRequest(int quantity)
 {
   // empty record
