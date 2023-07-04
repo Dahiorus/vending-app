b0f9cbe45ad82fa38346804fa3839267bee03f2c
Brondon Ung <bung@ilex-international.com>
2023-07-04 11:45:43 +0200

    Rename class

--- web/src/main/java/me/dahiorus/project/vending/VendingWebApplication.java
+++ web/src/main/java/me/dahiorus/project/vending/VendingWebApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan(basePackages = "me.dahiorus.project.vending")
-public class VendingApplication
+public class VendingWebApplication
 {
   public static void main(final String[] args)
   {
-    SpringApplication.run(VendingApplication.class, args);
+    SpringApplication.run(VendingWebApplication.class, args);
   }
 }
