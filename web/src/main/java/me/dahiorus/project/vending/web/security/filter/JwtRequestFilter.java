a558c3c3b8bf69c4d0b5ea5a6eceec4c7f8dda34
Brondon Ung <bung@ilex-international.com>
2023-07-04 11:43:59 +0200

    Refactor JwtRequestFilter
    Add methods to enhanced readability of the class

--- web/src/main/java/me/dahiorus/project/vending/web/security/filter/JwtRequestFilter.java
+++ web/src/main/java/me/dahiorus/project/vending/web/security/filter/JwtRequestFilter.java
@@ -2,17 +2,16 @@ package me.dahiorus.project.vending.web.security.filter;
 
 import java.io.IOException;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import me.dahiorus.project.vending.domain.exception.AppRuntimeException;
@@ -29,32 +28,34 @@ public class JwtRequestFilter extends OncePerRequestFilter
   private final JwtService jwtService;
 
   @Override
-  protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
-    final FilterChain filterChain)
+  protected void doFilterInternal(final HttpServletRequest request,
+    final HttpServletResponse response, final FilterChain filterChain)
     throws ServletException, IOException
   {
-    if (StringUtils.equalsAny(request.getServletPath(), SecurityConstants.AUTHENTICATE_ENDPOINT,
-      SecurityConstants.REFRESH_TOKEN_ENDPOINT))
+    if (isAuthenticateRequest(request))
     {
       log.trace("Requesting the authentication or the refresh token path");
       filterChain.doFilter(request, response);
       return;
     }
 
-    // get the header Authorization to get the JWT token
     String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-    // not authenticated, the request is not processed
-    if (!StringUtils.startsWith(authorizationHeader, AUTHORIZATION_HEADER_PREFIX))
+    if (!hasAuthorization(authorizationHeader))
     {
       log.debug("No header Authorization found with a Bearer token in the request");
       filterChain.doFilter(request, response);
       return;
     }
 
-    log.debug("Resolving the authenticated user from the request");
+    handleAuthenticatedUser(authorizationHeader);
+    filterChain.doFilter(request, response);
+  }
 
-    // parse the JWT token
+  private void handleAuthenticatedUser(String authorizationHeader)
+  {
+    log.debug("Resolving the authenticated user from the request");
+    
     String token = StringUtils.removeStart(authorizationHeader, AUTHORIZATION_HEADER_PREFIX);
     Authentication authentication;
     try
@@ -70,7 +71,16 @@ public class JwtRequestFilter extends OncePerRequestFilter
       .setAuthentication(authentication);
 
     log.debug("User from the request: {}", authentication);
+  }
 
-    filterChain.doFilter(request, response);
+  private static boolean hasAuthorization(String authorizationHeader)
+  {
+    return StringUtils.startsWith(authorizationHeader, AUTHORIZATION_HEADER_PREFIX);
+  }
+
+  private static boolean isAuthenticateRequest(final HttpServletRequest request)
+  {
+    return StringUtils.equalsAny(request.getServletPath(), SecurityConstants.AUTHENTICATE_ENDPOINT,
+      SecurityConstants.REFRESH_TOKEN_ENDPOINT);
   }
 }
