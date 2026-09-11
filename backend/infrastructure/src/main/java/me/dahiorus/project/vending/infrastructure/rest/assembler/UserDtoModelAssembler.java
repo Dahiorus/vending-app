3188680c4fd3e1ec3d43c6ac21616cd8d9f992ac
Brondon Ung <brondon.ung@vidal.fr>
2026-09-11 13:56:27 +0200

    feat: Enhance UserDtoModelAssembler with HATEOAS affordances for update and upload actions

--- backend/infrastructure/src/main/java/me/dahiorus/project/vending/infrastructure/rest/assembler/UserDtoModelAssembler.java
+++ backend/infrastructure/src/main/java/me/dahiorus/project/vending/infrastructure/rest/assembler/UserDtoModelAssembler.java
@@ -2,6 +2,7 @@ package me.dahiorus.project.vending.infrastructure.rest.assembler;
 
 import static me.dahiorus.project.vending.infrastructure.rest.assembler.Relation.SELF_PASSWORD;
 import static me.dahiorus.project.vending.infrastructure.rest.assembler.Relation.SELF_PICTURE;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.afford;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -22,19 +23,26 @@ public class UserDtoModelAssembler implements SimpleRepresentationModelAssembler
   @Override
   public void addLinks(final EntityModel<UserDto> resource) {
     Optional.ofNullable(resource.getContent())
-        .map(UserDtoModelAssembler::buildLinks)
+        .map(UserDtoModelAssembler::linksOf)
         .ifPresent(resource::add);
   }
 
-  private static Set<Link> buildLinks(UserDto content) {
+  private static Set<Link> linksOf(UserDto content) {
     return Set.of(
-        linkTo(methodOn(SelfServiceRestController.class).get(null)).withSelfRel(),
+        linkTo(methodOn(SelfServiceRestController.class).get(null))
+            .withSelfRel()
+            .andAffordance(afford(methodOn(SelfServiceRestController.class).update(null, null))),
+        linkTo(methodOn(SelfServiceRestController.class).updatePassword(null, null))
+            .withRel(SELF_PASSWORD)
+            .andAffordance(
+                afford(
+                    methodOn(SelfServiceRestController.class)
+                        .updatePassword(null, new EditPasswordRequestDto(null, null)))),
         linkTo(methodOn(SelfServiceRestController.class).getProfilePicture(null))
-            .withRel(SELF_PICTURE),
-        linkTo(
-                methodOn(SelfServiceRestController.class)
-                    .updatePassword(null, new EditPasswordRequestDto(null, null)))
-            .withRel(SELF_PASSWORD));
+            .withRel(SELF_PICTURE)
+            .andAffordance(
+                afford(
+                    methodOn(SelfServiceRestController.class).uploadProfilePicture(null, null))));
   }
 
   @Override
